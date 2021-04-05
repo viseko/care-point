26e20ee139451dd53a845499e83f880d3c26889f
Viseko <viseko23@gmail.com>
2021-04-06 01:00:16 +0700

    Старт проекта, разметка

---
+++ source/js/_webp.js
@@ -0,0 +1,13 @@
+// Взято здесь: https://gist.github.com/Protoff/d6643387f03d47b44b2d7c3cf7b3e0a0
+
+document.addEventListener('DOMContentLoaded', function() {
+  testWebP(document.body)
+})
+
+function testWebP(elem) {
+  const webP = new Image();
+  webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+  webP.onload = webP.onerror = function () {
+    webP.height === 2 ? elem.classList.add('webp') : elem.classList.add('no-webp')
+  }
+}
