329c55d1c4cb9ff74c0b33e31339177d989e2aa4
Viseko <viseko23@gmail.com>
2021-05-14 13:25:05 +0700

    Update 2021-05-14T06:24:59.098Z

--- js/script.js
+++ js/script.js
@@ -79,16 +79,7 @@ if (accordeons.length) {
     const accordeonBtn = accordeon.querySelector(".accordeon__title");
     const accordeonContent = accordeon.querySelector(".accordeon__content");
 
-    if (accordeon.open) {
-      accordeon.classList.add("_open");
-      accordeonContent.normalHeight = accordeonContent.offsetHeight + 'px';
-      accordeonContent.style.height = accordeonContent.normalHeight;
-    } else {
-      accordeon.open = true;
-      accordeonContent.normalHeight = accordeonContent.offsetHeight + 'px';
-      accordeon.open = false;
-      accordeonContent.style.height = "0px";
-    }
+    calcAccordeonNormalHeight(accordeon, accordeonContent);
 
     accordeonContent.addEventListener("transitionend", function() {
       if (accordeon.open && !accordeon.classList.contains("_open")) {
@@ -109,5 +100,192 @@ if (accordeons.length) {
       }
     });
   }
+}
+
+// Пересчитываем требуемую высоту открытых аккордионов при изменении ширины окна
+window.addEventListener("resize", function() {
+  if (accordeons.length) {
+    for (let i = 0; i < accordeons.length; i++) {
+      const accordeon = accordeons[i];
+      const accordeonContent = accordeon.querySelector(".accordeon__content");
+
+      calcAccordeonNormalHeight(accordeon, accordeonContent);
+    }
+  }
+});
+
+function calcAccordeonNormalHeight(accordeon, content) {
+  if (accordeon.open) {
+    if (!accordeon.classList.contains("_open")) {
+      accordeon.classList.add("_open");
+    }
+    content.style.height = null;
+    content.normalHeight = content.offsetHeight + 'px';
+    content.style.height = content.normalHeight;
+  } else {
+    accordeon.open = true;
+    content.style.height = null;
+    content.normalHeight = content.offsetHeight + 'px';
+    accordeon.open = false;
+    content.style.height = "0px";
+  }
+}
+;
+
+// Stats increment on scroll
+const increments = document.querySelectorAll('.js-stats-increment');
+const incrementSpeed = 100;
+
+// Определяем свойства инкрементов
+for (let i = 0; i < increments.length; i++) {
+  const increment = increments[i];
+  increment.plus = increment.nextElementSibling;
+  increment.totalNumber = +increment.innerHTML;
+  increment.visible = false;
+  increment.innerHTML = '0';
+
+  increment.style.cssText = 'transition: 0.5s; opacity: 0;';
+  if (increment.plus) {
+    increment.plus.style.cssText = 'transition: 0.2s; opacity: 0;';
+  }
+}
+
+// При скролле, отображаем инкременты если они попадают в область просмотра
+let isScrolling = false;
+
+window.addEventListener('scroll', function(e) {
+  if (!isScrolling) {
+    window.requestAnimationFrame(function() {
+      checkIncrementsReach(e);
+      isScrolling = false;
+    });
+  }
+  isScrolling = true;
+});
+
+
+function checkIncrementsReach(e) {
+  for (let i = 0; i < increments.length; i++) {
+    const increment = increments[i];
+    if (!increment.visible && isElementFullVisible(increment)) {
+      showIncrement(increment)
+    }
+  }
+}
+
+// Проверка
+function isElementFullVisible(el) {
+  const elementBoundary = el.getBoundingClientRect();
+  const top = elementBoundary.top;
+  const bottom = elementBoundary.bottom;
+
+  return ((top >= 0) && (bottom <= window.innerHeight - window.innerHeight / 10));
+}
+
+function showIncrement(increment) {
+  increment.style.opacity = '1';
+  increment.visible = true;
+
+  let timer = setInterval(function() {
+    const currentNumber = +increment.innerHTML;
+    if (currentNumber != increment.totalNumber) {
+      increment.innerHTML = Math.min(
+        currentNumber + Math.floor(increment.totalNumber/incrementSpeed),
+        increment.totalNumber);
+    } else {
+      clearTimeout(timer);
+      if (increment.plus) {
+        increment.plus.style.opacity = '1';
+      }
+    }
+  }, 10);
+}
+;
+
+//Gallery slider for mobile
+let gallerySlider = null;
+const mobileBreakpoint = window.matchMedia("(min-width: 501px)");
+
+mobileBreakpoint.addEventListener("change", mobileBreakpointChecker);
+mobileBreakpointChecker();
+
+function mobileBreakpointChecker() {
+  if (mobileBreakpoint.matches) {
+    if (gallerySlider) {
+      gallerySlider.destroy();
+    }
+  } else {
+    initGallerySlider();
+  }
+}
+
+function initGallerySlider() {
+  gallerySlider = new Swiper(".js-gallery-slider", {
+    wrapperClass: "js-gallery-list",
+    slideClass: "js-gallery-slide",
+
+    direction: "horizontal",
+    loop: false,
+    slidesPerView: "1",
+
+    pagination: {
+      el: ".js-gallery__pagination",
+      bulletClass: "slider__bullet",
+      bulletActiveClass: "slider__bullet--active",
+      bulletElement: "button",
+      clickable: true,
+      clickableClass: "slider__pagination__clickable"
+    },
+
+    navigation: {
+      nextEl: ".js-gallery-next",
+      prevEl: ".js-gallery-prev",
+      disabledClass: "slider__nav--disabled",
+    },
+
+    followFinger: true
+  });
+}
+;
+
+//Form validation and modal window
+
+maskPhone("#form-phone");
+// Источник: https://github.com/KatrinaNov/maskPhone
+function maskPhone(selector, masked = '+7 (___) ___-__-__') {
+	const elems = document.querySelectorAll(selector);
+
+	function mask(event) {
+		const keyCode = event.keyCode;
+		const template = masked,
+			def = template.replace(/\D/g, ""),
+			val = this.value.replace(/\D/g, "");
+		let i = 0,
+			newValue = template.replace(/[_\d]/g, function (a) {
+				return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+			});
+		i = newValue.indexOf("_");
+		if (i !== -1) {
+			newValue = newValue.slice(0, i);
+		}
+		let reg = template.substr(0, this.value.length).replace(/_+/g,
+			function (a) {
+				return "\\d{1," + a.length + "}";
+			}).replace(/[+()]/g, "\\$&");
+		reg = new RegExp("^" + reg + "$");
+		if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+			this.value = newValue;
+		}
+		if (event.type === "blur" && this.value.length < 5) {
+			this.value = "";
+		}
+
+	}
+
+	for (const elem of elems) {
+		elem.addEventListener("input", mask);
+		elem.addEventListener("focus", mask);
+		elem.addEventListener("blur", mask);
+	}
 }
 ;
